e2b101d47cc4e4f6b3258613ef94cc359653cb56
seiji <pillow1207@gmail.com>
2021-06-20 23:24:30 +0900

    独自例外を捕捉。

--- src/main/java/com/example/app/inquiry/InquiryController.java
+++ src/main/java/com/example/app/inquiry/InquiryController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.app.entity.Inquiry;
+import com.example.service.InquiryNotFoundException;
 import com.example.service.InquiryService;
 
 @Controller
@@ -31,6 +32,20 @@ public class InquiryController {
 	@GetMapping
 	public String index(Model model) {
 		List<Inquiry> list = inquiryService.getAll();
+		
+		Inquiry inquiry = new Inquiry();
+		inquiry.setId(4);
+		inquiry.setName("Jamie");
+		inquiry.setEmail("sample4@example.com");
+		inquiry.setContents("Hello");
+		
+		try {
+			inquiryService.update(inquiry);
+		} catch (InquiryNotFoundException e) {
+			model.addAttribute("message", e);
+			return "error/CustomPage";
+		}
+		
 		model.addAttribute("inquiryList", list);
 		model.addAttribute("title", "Inquiry Index");
 		return "inquiry/index";
